7800060be2beacc3fa38f5fd41aabae49fff25b4
race4space <chris.owtram@gmail.com>
2021-05-30 21:15:56 +0200

    xxx

--- fileexist.php
+++ fileexist.php
@@ -1,7 +1,7 @@
 <?php
 
 $str_path_server=$_SERVER['DOCUMENT_ROOT'];
-$str_path_folder=$str_path_server."/xdesign";
+$str_path_folder=$str_path_server."/xdesign-projects/MyProject";
 
 echo("str_path_server: ".$str_path_server."<br>");
 echo("str_path_folder: ".$str_path_folder."<br>");
