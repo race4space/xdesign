6330e0feeaa1d83650de1ed0bc4c3b5976e25d93
race4space <chris.owtram@gmail.com>
2021-05-31 16:41:36 +0200

    xxx

--- fileexist.php
+++ fileexist.php
@@ -1,12 +1,12 @@
 <?php
 
-$str_datetime=date("Y-m-d-H-i-s");                  
+$str_UniqueName=date("Y-m-d-H-i-s");                  
 
 $str_path_server=$_SERVER['DOCUMENT_ROOT'];      
 $folderpath_designInstance=$str_path_server."/xdesign";
 
 $folderpath_projectInstance=$folderpath_designInstance."/AFrame";
-//$folderpath_projectInstance.="/".$str_datetime;
+//$folderpath_projectInstance.="/".$str_UniqueName;
 
 
 $folderpath_projectDestination=$str_path_server."/MyProject";            
